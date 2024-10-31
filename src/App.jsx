dffe5f022d8090a8be19e64dd492950f5e60df68
katya-test <katyasuhr@gmail.com>
2024-10-30 20:53:41 -0400

    List creation

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+/**
+ *  Inside your unordered list, insert a JavaScript expression
+hint: {}
+ Inside the JavaScript expression, map over your todoList array
+ For each Object in the Array, return a list item (<li>) with the following:
+key attribute with value of id property
+inner text content with value of title property
+ */
 
+let todoList = [
+  {"id": 111, "title": "Complete assignment"},
+  {"id": 112, "title": "Submit assignment"},
+  {"id": 113, "title": "Attend mentor session"}
+];
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>ToDo List</h1>
+      <ul>
+        {todoList.map(function(list) {
+            return <li key={list.id}>{list.title}</li>;
+          })
+        }
+      </ul>
+    </div> );
 }
 
-export default App
+export default App;

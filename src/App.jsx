dce3a0e89f457eb91a7087a04a929abe1dd50bda
katya-test <katyasuhr@gmail.com>
2024-11-04 13:41:32 -0500

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-
-/**
- *  Inside your unordered list, insert a JavaScript expression
-hint: {}
- Inside the JavaScript expression, map over your todoList array
- For each Object in the Array, return a list item (<li>) with the following:
-key attribute with value of id property
-inner text content with value of title property
- */
-
-let todoList = [
-  {"id": 111, "title": "Complete assignment"},
-  {"id": 112, "title": "Submit assignment"},
-  {"id": 113, "title": "Attend mentor session"}
-];
+import TodoList from './ToDoList'
+import AddTodoForm from './AddTodoForm';
 
 function App() {
   return (
     <div>
       <h1>ToDo List</h1>
-      <ul>
-        {todoList.map(function(list) {
-            return <li key={list.id}>{list.title}</li>;
-          })
-        }
-      </ul>
+      <AddTodoForm/>
+      <TodoList/>
     </div> );
 }
 

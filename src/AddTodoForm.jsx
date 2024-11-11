44b7a6f1765138f325d4b6d6a651941bb9edf829
katya-test <katyasuhr@gmail.com>
2024-11-11 10:36:06 -0500

    issues fixed

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -6,14 +6,13 @@ function AddTodoForm(props) {
         const todoTitle = event.target.title.value;
         console.log(todoTitle);
         props.onAddTodo(todoTitle);
-        // props.setNewTodo(todoTitle);
         event.target.reset();
     }    
     return (
-        <form>
+        <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Title</label>
-            <input id='todoTitle' name='title' onSubmit={handleAddTodo}></input>
-            <button>Add</button>
+            <input id='todoTitle' name='title'></input>
+            <button type='submit'>Add</button>
         </form>
     );
     

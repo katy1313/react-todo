138fd705101836ec2466a6523f3f1cf70927202f
katya-test <katyasuhr@gmail.com>
2024-11-20 21:15:02 -0500

    hooks added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css'
 import TodoList from './ToDoList'
 import AddTodoForm from './AddTodoForm';
 
+function useSemiPersistentState() {
+  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList'))); //Reading the data from local storage when application rerenders
+
+  React.useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList)); //Converting Object to a string before saving in localStorage
+  }, [todoList]);
+  return [todoList, setTodoList];
+}
+
 function App() {
 
-  const [todoList, setTodoList] = React.useState([]);//Creating new state variable named todoList with setter setTodoList and default value of an empty Array
+  const [todoList, setTodoList] = useSemiPersistentState(); //Using custom hook
 
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]); //update todoList to include newTodo along with existing items
   }
 
   return (
-    <div>
+    <Fragment>
       <h1>ToDo List</h1>
       <AddTodoForm onAddTodo={addTodo}/>
       <TodoList list={todoList} todoList={todoList} setTodoList={setTodoList} />
-    </div> );
+    </Fragment> );
 }
 
 

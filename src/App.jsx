fa00267d85f5ac8bbf8e9125a83bcb58d9a9f1ef
katya-test <katyasuhr@gmail.com>
2024-12-06 16:17:59 -0500

    loading added

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,29 @@ import './App.css'
 import TodoList from './ToDoList'
 import AddTodoForm from './AddTodoForm';
 
-function useSemiPersistentState() {
+function App() {
   const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList')) || [] ); //Reading the data from local storage when application rerenders
 
+  const [isLoading, setIsLoading] = React.useState(true);
+
   React.useEffect(() => {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList)); //Converting Object to a string before saving in localStorage
-  }, [todoList]);
-  return [todoList, setTodoList];
-}
+    //Simulating a delay with setTimeout in a promise
 
-function App() {
+    new Promise(function(resolve) {
+      setTimeout(() => resolve({ data: { todoList: todoList } }), 2000);
+    })
+    .then((result) => {
+      setTodoList(result.data.todoList); // Update todoList after "fetching" data
+      setIsLoading(false); //Turn off loading
+    });
+  }, []);
 
-  const [todoList, setTodoList] = useSemiPersistentState(); //Using custom hook
+  React.useEffect(() => {
+    if(!isLoading) {
+      localStorage.setItem('savedTodoList', JSON.stringify(todoList)); //Save data to localStorage and converting Object to a string before saving in localStorage
+    }
+  }, [todoList, isLoading]);
+  
 
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]); //update todoList to include newTodo along with existing items
@@ -32,7 +43,13 @@ function App() {
     <Fragment>
       <h1>ToDo List</h1>
       <AddTodoForm onAddTodo={addTodo}/>
-      <TodoList list={todoList} todoList={todoList} setTodoList={setTodoList} onRemoveTodo={removeTodo}/>
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <TodoList list={todoList} todoList={todoList} setTodoList={setTodoList} onRemoveTodo={removeTodo}/>
+      )}
+      
+      <hr></hr>
     </Fragment> );
 }
 
